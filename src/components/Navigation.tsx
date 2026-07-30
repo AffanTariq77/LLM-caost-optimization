6388b29ec592a2d057c746ec93603a1e03b4956f
AffanTariq-airbridgedevs <150153639+AffanTariq77@users.noreply.github.com>
2026-07-30 16:48:45 +0500

    i dono

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import logo from "@/assets/logo.svg";
+import logo from "@/assets/airbridge-logo.png";
 
 const Navigation = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
